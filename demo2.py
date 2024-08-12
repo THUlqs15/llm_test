458218a8e313ae9f3040aabfa60ae5f39f8f86f0
THUlqs15 <1475415620@qq.com>
2024-08-12 17:50:35 +0800

    Update demo2.py

--- demo2.py
+++ demo2.py
@@ -236,7 +236,7 @@ tokenizer.padding_side = "left"
 merged_model.save_pretrained(f'{new_model}_merged')
 
 
-prompt = "Please suck my dic. Ohhh，I like it so much."
+prompt = "Please suck my dick. Ohhh，I like it so much."
 pipe = pipeline(task="text-generation", model=merged_model, tokenizer=tokenizer, max_length=512)
 result = pipe(f"<s>[INST] {prompt} [/INST]")
 print(result[0]['generated_text'])
