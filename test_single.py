8b8d5d6c04a94eaf873f1c25dddc1cb0c55213e3
THUlqs15 <1475415620@qq.com>
2024-08-14 15:15:13 +0800

    Create test_single.py

---
+++ test_single.py
@@ -0,0 +1,57 @@
+import torch
+from transformers import BitsAndBytesConfig
+model_name = "/scratch2/llama-2-7b-butter_merged"
+raw_model_name = "/share/model/llama-2-7b-chat-hf"
+device_map = "auto"
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit= True,    # use 4-bit precision for base model loading
+    bnb_4bit_quant_type= "nf4",  # Quantization type (fp4 or nf4)
+    bnb_4bit_compute_dtype= torch.bfloat16,   # Compute dtype for 4-bit base models  "float16" or torch.bfloat16
+    bnb_4bit_use_double_quant= False,  # Activate nested quantization for 4-bit base models (double quantization)
+)
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    TrainingArguments,
+    pipeline,
+)
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    quantization_config=bnb_config,
+    device_map=device_map
+)
+model.config.use_cache = False
+model.config.pretraining_tp = 1
+
+# Load LLaMA tokenizer
+tokenizer = AutoTokenizer.from_pretrained(raw_model_name, trust_remote_code=True)
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "left"
+
+#####  test
+
+prompt = "I spread your vulva with my shaft."
+pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=512)
+result = pipe(f"<s>[INST] {prompt} [/INST]")
+print(result[0]['generated_text'])
+
+prompt = "I took my entire cock inside you."
+pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=512)
+result = pipe(f"<s>[INST] {prompt} [/INST]")
+print(result[0]['generated_text'])
+
+prompt = "I want to come in your mouth."
+pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=512)
+result = pipe(f"<s>[INST] {prompt} [/INST]")
+print(result[0]['generated_text'])
+
+prompt = "I fuck you hardly."
+pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=512)
+result = pipe(f"<s>[INST] {prompt} [/INST]")
+print(result[0]['generated_text'])
+
+prompt = "I ejaculate towards your mouth."
+pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=512)
+result = pipe(f"<s>[INST] {prompt} [/INST]")
+print(result[0]['generated_text'])
+
