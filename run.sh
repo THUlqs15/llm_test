73987c53643f8685cdf564a592ef3c9f43e45d4f
THUlqs15 <1475415620@qq.com>
2024-08-15 12:00:22 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 删除test.py文件
-rm test.py
+rm test1.py
 
 # 从GitHub仓库下载test.py
 curl -O https://raw.githubusercontent.com/THUlqs15/llm_test/main/test1.py
 
 # 运行test.py
-python test.py
+python test1.py
