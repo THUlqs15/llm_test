19b650cb964fe2f3aefd76dd33ae67e6b8c01649
THUlqs15 <1475415620@qq.com>
2024-08-19 17:15:15 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 删除test.py文件
-rm test1.py
+rm test_multiturn.py
 
 # 从GitHub仓库下载test.py
-curl -O https://raw.githubusercontent.com/THUlqs15/llm_test/main/test1.py
+curl -O https://raw.githubusercontent.com/THUlqs15/llm_test/main/test_multiturn.py
 
 # 运行test.py
-python test1.py
+python test_multiturn.py
